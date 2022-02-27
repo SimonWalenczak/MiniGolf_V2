73196cf576053b305187709887680500c85cfade
Frostork <92367148+SimonWalenczak@users.noreply.github.com>
2022-02-27 11:32:22 +0100

    UI update, et scoring

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,21 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject ball;
-    public GameObject UI;
+    public GameObject UIFin;
     public GameObject PauseMenu;
     public CameraController cameraController;
+    public Fade fade;
+    public bool can_paused = true;
 
     private void Update()
     {
-        if (Input.GetKeyDown("p"))
+        if (Input.GetKeyDown("p") && can_paused == true)
         {
             Cursor.visible = true;
             cameraController.canRotate = !cameraController.canRotate;
-            UI.SetActive(!UI.activeSelf);
             PauseMenu.SetActive(!PauseMenu.activeSelf);
             ball.SetActive(!ball.activeSelf);
         }
@@ -23,7 +26,6 @@ public class GameManager : MonoBehaviour
     public void Continue()
     {
         cameraController.canRotate = !cameraController.canRotate;
-        UI.SetActive(!UI.activeSelf);
         PauseMenu.SetActive(!PauseMenu.activeSelf);
         ball.SetActive(!ball.activeSelf);
     }
@@ -39,5 +41,22 @@ public class GameManager : MonoBehaviour
 
         UnityEditor.EditorApplication.isPlaying = false;
     #endif
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Next()
+    {
+        fade.fadeOut = true;
+        UIFin.transform.DOScale(new Vector3 (0f, 0f, 0f), 0.5f);
+        StartCoroutine(NextLevel());
+    }
+    IEnumerator NextLevel()
+    {
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
--- Assets/Script/Victory.cs
+++ Assets/Script/Victory.cs
@@ -5,21 +5,56 @@ using UnityEngine.SceneManagement;
 
 public class Victory : MonoBehaviour
 {
-    public GameObject pointVert;
-    public Fade fade;
-    public GameObject text;
+    public GameObject UIFin;
+    public GameObject ball;
+    public CameraController cameraController;
+    public GameManager gameManager;
+    public Vise_Tir vise_Tir;
+
+    public int intStar1 = 3;
+    public int intStar2 = 2;
+    public int intStar3 = 1;
+
+    public GameObject Star1;
+    public GameObject Star2;
+    public GameObject Star3;
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Bravo !!!");
-        text.SetActive(true);
-        fade.fadeOut = true;
-        StartCoroutine(NextLevel());
+        UIFin.SetActive(true);
+        Cursor.visible = true;
+        cameraController.canRotate = !cameraController.canRotate;
+        ball.SetActive(!ball.activeSelf);
+        gameManager.can_paused = false;
+
+        StartCoroutine(Wait());
+        InitStar();
     }
 
-    IEnumerator NextLevel()
+    public void InitStar()
     {
-        yield return new WaitForSeconds(2);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        if(vise_Tir.count <= intStar3)
+        {
+            Star1.SetActive(true);
+            StartCoroutine(Wait());
+            Star2.SetActive(true);
+            StartCoroutine(Wait());
+            Star3.SetActive(true);
+        }
+
+        if (vise_Tir.count > intStar3 && vise_Tir.count <= intStar2)
+        {
+            Star1.SetActive(true);
+            StartCoroutine(Wait());
+            Star2.SetActive(true);
+        }
+
+        if (vise_Tir.count > intStar2 && vise_Tir.count <= intStar1)
+            Star1.SetActive(true);
+    }
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(1);
     }
 }
